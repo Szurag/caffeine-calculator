8beb6360efe215ac53e1c9d55592d0264bb340d5
Szurag <bartek@stco.cloud>
2025-10-20 01:49:59 +0200

    draft

--- products/urls.py
+++ products/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.index, name='product_index'),
     path('add/', views.add, name='product_add'),
     path('<int:product_id>/', views.product_detail, name='product_detail'),
+    path('delete/<int:product_id>/', views.delete_product, name='product_delete'),
 ]
 
